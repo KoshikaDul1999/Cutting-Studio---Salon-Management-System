66ab4fb9fb329774fea8dd7caadc61feceb1220c
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-01-02 16:15:02 +0530

    complete user registration and login

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import authRoutes from "./routes/auth.js"
+import cors from "cors"
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
 app.use(express.json())
+app.use(
+    cors({
+        origin: "http://localhost:3000"
+    })
+)
+app.use(cookieParser())
+
 app.use("/api/auth", authRoutes)
 
 app.listen(8080, ()=>{
