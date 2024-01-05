7f0b86d428a132cd2658c3b6b3ded4905b51db5c
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-01-05 16:04:37 +0530

    update app.js and add libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,48 @@ import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import Home from "./pages/home/Home";
 
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Outlet,
   Navigate,
+  RouterProvider,
+  createBrowserRouter,
 } from "react-router-dom";
+
 import './App.css';
 
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
   const { darkMode } = useContext(DarkModeContext);
+
+  const queryClient = new QueryClient();
+
+  const Layout = () => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <div className={`theme-${darkMode ? "dark" : "light"}`}>
+          <Navbar />
+          <div style={{ display: "flex" }}>
+            <LeftBar />
+            <div style={{ flex: 6 }}>
+              <Outlet />
+            </div>
+          </div>
+        </div>
+      </QueryClientProvider>
+    );
+  };
 
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
@@ -27,15 +54,46 @@ function App() {
     return children;
   };
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-      </Routes>
-    </Router>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
+
+  // return (
+  //   <Router>
+  //     <Routes>
+  //       <Route path="/login" element={<Login />} />
+  //       <Route path="/register" element={<Register />} />
+  //       <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+  //     </Routes>
+  //   </Router>
+  // );
 }
 
 export default App;
