cf952d4d3f7051a0235f9974408dbab1b5d0c020
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-01-06 15:48:04 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,8 @@ import Home from "./pages/home/Home";
 
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 import './App.css';
 
@@ -24,63 +17,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 function App() {
   const { currentUser } = useContext(AuthContext);
   const { darkMode } = useContext(DarkModeContext);
-
-  const queryClient = new QueryClient();
-
-  const Layout = () => {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <div className={`theme-${darkMode ? "dark" : "light"}`}>
-          <div style={{ display: "flex" }}>
-            <Sidebar />
-            <div style={{ flex: 6 }}>
-              <Navbar />
-              <Outlet />
-            </div>
-          </div>
-        </div>
-      </QueryClientProvider>
-    );
-  };
   
-
-  const ProtectedRoute = ({ children }) => {
-    if (!currentUser) {
-      // Redirect to the login page if the user is not authenticated
-      return <Navigate to="/login" />;
-    }
-
-    return children;
-  };
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-  ]);
-
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 
