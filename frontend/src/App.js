6b8aae357710731709079988fadfa6268d6f71ef
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-01-10 12:14:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
+import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
@@ -24,7 +26,12 @@ function App() {
             <Route path="users">
               <Route index element={<List />}/>
               <Route path=":userId" element={<Single />}/>
-              <Route path="new" element={<New />}/>
+              <Route path="new" element={<New inputs={userInputs} title="Add New User" />}/>
+            </Route>
+            <Route path="products">
+              <Route index element={<List />}/>
+              <Route path=":productId" element={<Single />}/>
+              <Route path="new" element={<New inputs={productInputs} title="Add New Products" />}/>
             </Route>
           </Route>
         </Routes>
