dc8240d282a7e962d4ebd10d0c01c513c7b25e5d
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2024-01-10 11:22:16 +0530

    salesModule

---
+++ backend/models/SalesModule.js
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../connect.js';
+
+const Sale = sequelize.define('sales', {
+  SaleID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  description: {
+    type: DataTypes.STRING(255),
+  },
+  amount: {
+    type: DataTypes.DOUBLE(10, 2),
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false,
+  },
+});
+
+export default Sale;
