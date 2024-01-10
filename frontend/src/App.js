dc8c2c7cec1340a281893898759a0668bed0c6f9
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-01-10 11:26:35 +0530

    Update App.js and add libraires

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
@@ -18,6 +21,11 @@ function App() {
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="users">
+              <Route index element={<List />}/>
+              <Route path=":userId" element={<Single />}/>
+              <Route path="new" element={<New />}/>
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
